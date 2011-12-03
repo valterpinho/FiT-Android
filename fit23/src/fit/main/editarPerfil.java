7650919ddd9d5a87ae706a1ac3f338655e1b547f
Andre Marinho <andrebcmarinho@gmail.com>
2011-12-03 16:17:58 +0000

    Validacao de campos em editar perfil. Pequenas alteracoes no xml do layout.

--- fit23/src/fit/main/editarPerfil.java
+++ fit23/src/fit/main/editarPerfil.java
@@ -42,43 +42,68 @@ public class editarPerfil extends Activity {
 	private OnClickListener btn_save_listener = new OnClickListener() {
 		public void onClick(View v) {
 
-			String extension = "users/edit.xml";
-			String rNode = "edit";
-			String[] fields = {"token", "datanascimento", "email", "morada", "nome", "telefone"};
-			String[] values = {userID, et_datanasc.getText().toString(), et_email.getText().toString(), et_morada.getText().toString(), et_nome.getText().toString(), et_telefone.getText().toString()};
-			String[] responseFields = {"message"};
-			ArrayList<String> response = null;
-			try {
-				response = Utils.POST(extension, rNode, responseFields, fields, values);
+			String datanasc = et_datanasc.getText().toString(), email = et_email.getText().toString();
+			String em[] = email.split("@");
+			String dn[] = datanasc.split("-");
+			
+			//email tem de ter @ e . na string a seguir ao @
+			//primeira parte tem de ter no minimo 3 caracteres
+			if(em[0].length() < 3 || em.length != 2){
+				Toast t = Toast.makeText(getApplicationContext(),
+						"Email inválido!",
+						Toast.LENGTH_SHORT);
+				t.show();
+			}
+			//data de nascimento tem de ter 3 partes (dia,ano,mes)
+			//e cada uma das partes tem de ter no minimo 2 numeros e no maximo 4
+			else if(dn.length != 3 
+					|| (dn[0].length() < 2 || dn[0].length() > 4)
+					|| (dn[1].length() < 2 || dn[1].length() > 4)
+					|| (dn[2].length() < 2 || dn[2].length() > 4)){
+				Toast t = Toast.makeText(getApplicationContext(),
+						"Data de nascimento inválida!",
+						Toast.LENGTH_SHORT);
+				t.show();
+			}else{
 
-				if(response.get(0).equals("success")){
+				String extension = "users/edit.xml";
+				String rNode = "edit";
+				String[] fields = {"token", "datanascimento", "email", "morada", "nome", "telefone"};
+				String[] values = {userID, datanasc, email, et_morada.getText().toString(), et_nome.getText().toString(), et_telefone.getText().toString()};
+				String[] responseFields = {"message"};
+				ArrayList<String> response = null;
+				try {
+					response = Utils.POST(extension, rNode, responseFields, fields, values);
 
-					Intent i = new Intent(editarPerfil.this, perfil.class);
+					if(response.get(0).equals("success")){
 
-					if(response != null){
-						Bundle b = new Bundle();
-						b.putString("user-id", userID);
-						i.putExtras(b);
+						Intent i = new Intent(editarPerfil.this, perfil.class);
+
+						if(response != null){
+							Bundle b = new Bundle();
+							b.putString("user-id", userID);
+							i.putExtras(b);
+						}
+
+						startActivity(i);					
 					}
+					else{
 
-					startActivity(i);					
-				}
-				else{
-
+						AlertDialog.Builder infoResultado = new AlertDialog.Builder(editarPerfil.this);
+						infoResultado.setTitle("Erro");
+						infoResultado.setMessage(response.get(0));
+						infoResultado.setNeutralButton("Ok",null);
+						infoResultado.show();
+						response.clear();
+					}					
+				} catch (Exception e){
 					AlertDialog.Builder infoResultado = new AlertDialog.Builder(editarPerfil.this);
 					infoResultado.setTitle("Erro");
-					infoResultado.setMessage(response.get(0));
+					infoResultado.setMessage("Erro inesperado");
 					infoResultado.setNeutralButton("Ok",null);
 					infoResultado.show();
 					response.clear();
-				}					
-			} catch (Exception e){
-				AlertDialog.Builder infoResultado = new AlertDialog.Builder(editarPerfil.this);
-				infoResultado.setTitle("Erro");
-				infoResultado.setMessage("Erro inesperado");
-				infoResultado.setNeutralButton("Ok",null);
-				infoResultado.show();
-				response.clear();
+				}
 			}
 		}
 	};	
