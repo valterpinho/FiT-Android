eee3afa98f7414915c7470a9e1ad21bc3ee2f045
Andre Marinho <andrebcmarinho@gmail.com>
2011-12-04 18:07:01 +0000

    Utiliza o GPS para mostrar a posicao actual no mapa.

--- fit23/src/fit/main/map.java
+++ fit23/src/fit/main/map.java
@@ -2,8 +2,15 @@ package fit.main;
 
 import java.util.List;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
@@ -15,9 +22,11 @@ import com.google.android.maps.OverlayItem;
 public class map extends MapActivity {
 
 	private MapView mapView;
+	private GeoPoint myLocation = null;
+	Location current = null;
 
-	private static final int gym_lat_dragao = 41161427;
-	private static final int gym_long_dragao = -8582106;
+	//private static final int gym_lat_dragao = 41161427;
+	//private static final int gym_long_dragao = -8582106;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -25,29 +34,95 @@ public class map extends MapActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.map);
 
+		verificaLigacoes();
+
+	}
+
+	void criaMapa(){
 		mapView = (MapView) findViewById(R.id.map_view);       
 		mapView.setBuiltInZoomControls(true);
+		mapView.setSatellite(true); //new
 
 		List<Overlay> mapOverlays = mapView.getOverlays();
 		Drawable drawable = this.getResources().getDrawable(R.drawable.plano_treino);
 		map_overlay itemizedOverlay = new map_overlay(drawable, this);
 
-		GeoPoint point = new GeoPoint(gym_lat_dragao, gym_long_dragao);
-		OverlayItem overlayitem = new OverlayItem(point, "Solinca Dragão", "Telef:  22 110 11 01");
+		getLocation();
 
-		itemizedOverlay.addOverlay(overlayitem);
-		mapOverlays.add(itemizedOverlay);
+		if(myLocation != null){
+			OverlayItem overlayitem = new OverlayItem(myLocation, "Solinca Dragão", "Telef:  22 110 11 01");
 
-		MapController mapController = mapView.getController();
+			itemizedOverlay.addOverlay(overlayitem);
+			mapOverlays.add(itemizedOverlay);
 
-		mapController.animateTo(point);
-		mapController.setZoom(16);
+			MapController mapController = mapView.getController();
 
+			mapController.animateTo(myLocation);
+			mapController.setZoom(16);
+		}
+	}
+
+	//vai buscar a posicao atual do telemovel pela net ou por gps (dependendo do servico que estiver diponivel)
+	void getLocation(){
+		LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		List<String> prov = locationManager.getAllProviders();
+
+		if(prov.size()>0){
+			for(String s : prov)
+			{
+				if(locationManager.isProviderEnabled(s))
+					current = locationManager.getLastKnownLocation(s);
+			}
+		}
+		if(current!=null){
+			int lat = (int) (current.getLatitude() * 1E6);
+			int lng = (int) (current.getLongitude() * 1E6);
+			myLocation = new GeoPoint(lat, lng);
+		}
 	}
 
 	@Override
 	protected boolean isRouteDisplayed() {
 		return false;
+	}
+
+	void verificaLigacoes(){
+		ConnectivityManager manager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+		boolean wifi = manager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnected();
+		boolean mobile = manager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).isConnected();
+		LocationManager locManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+		boolean gps = locManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+		boolean net = locManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage("É necessário uma ligação à Internet. Verifique as suas definições.")
+		.setCancelable(false)
+		.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				finish();
+			}
+		});
+
+		AlertDialog.Builder builder2 = new AlertDialog.Builder(this);
+		builder2.setMessage("É necessário encontrar a sua posição actual através do GPS ou redes sem fios. Verifique as suas definições.")
+		.setCancelable(false)
+		.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				finish();
+			}
+		});
+
+		if(wifi || mobile)
+			if(gps || net)
+				criaMapa();
+			else{
+				AlertDialog alert = builder2.create();
+				alert.show();
+			}
+		else{
+			AlertDialog alert = builder.create();
+			alert.show();
+		}
 	}
 
 }
